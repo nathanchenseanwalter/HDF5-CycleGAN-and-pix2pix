d1939e53c9723b4a267a3059e32c7045c00d8a86
Nathaniel Chen <79379842+nathanchenseanwalter@users.noreply.github.com>
2024-01-02 17:50:43 -0800

    HDF5 Compatibility with Plasma TV

---
+++ data/hdf5_dataset.py
@@ -0,0 +1,62 @@
+import h5py
+import numpy as np
+import torch
+from pathlib import Path
+from data.base_dataset import BaseDataset
+
+"""""
+# Example usage: Put tv_synth.h5 in main folder and run:
+
+python train.py --dataroot tv_synth.h5 --name tv_synth_mdl --model pix2pix --direction BtoA --dataset_mode hdf5 --gpu_ids -1 --input_nc 1 --output_nc 1
+
+"""""
+
+class HDF5Dataset(BaseDataset):
+    """A dataset class for paired image dataset.
+
+    It assumes that the directory '/path/to/data/train' contains image pairs in the form of {A,B}.
+    During test time, you need to prepare a directory '/path/to/data/test'.
+    """
+
+    def __init__(self, opt):
+        """Initialize this dataset class.
+
+        Parameters:
+            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        BaseDataset.__init__(self, opt)
+        self.AB_path = Path(opt.dataroot) # get the image directory
+        print(self.AB_path)
+        with h5py.File(self.AB_path, 'r') as f:
+            print(list(f.keys()))
+            self.A_arr = f['synth_' + opt.phase][:]
+            self.B_arr = f['tv_' + opt.phase][:]
+
+    def __getitem__(self, index):
+        """Return a data point and its metadata information.
+
+        Parameters:
+            index - - a random integer for data indexing
+
+        Returns a dictionary that contains A, B, A_paths and B_paths
+            A (tensor) - - an image in the input domain
+            B (tensor) - - its corresponding image in the target domain
+            A_paths (str) - - image paths
+            B_paths (str) - - image paths (same as A_paths)
+        """
+        
+        # read a image given a random integer index
+        A = self.A_arr[index]
+        B = self.B_arr[index]
+        
+        A = np.expand_dims(A, axis=0)
+        B = np.expand_dims(B, axis=0)
+        
+        A = torch.from_numpy(A).float()
+        B = torch.from_numpy(B).float()
+
+        return {'A': A, 'B': B, 'A_paths': str(self.AB_path.stem), 'B_paths': str(self.AB_path.stem)}
+
+    def __len__(self):
+        """Return the total number of images in the dataset."""
+        return len(self.A_arr)
