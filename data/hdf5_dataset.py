8f838c763304a7ed3fbcbd5f59872f3252bd1242
Nathaniel Chen <79379842+nathanchenseanwalter@users.noreply.github.com>
2024-01-08 13:18:30 -1000

    dd

--- data/hdf5_dataset.py
+++ data/hdf5_dataset.py
@@ -7,8 +7,9 @@ from data.base_dataset import BaseDataset
 """""
 # Example usage: Put tv_synth.h5 in main folder and run:
 
-python train.py --dataroot tv_synth.h5 --name tv_synth_mdl --model pix2pix --direction BtoA --dataset_mode hdf5 --gpu_ids -1 --input_nc 1 --output_nc 1
+python train.py --dataroot tv_synth.h5 --name tv_synth_mdl --model pix2pix --norm instance --direction BtoA --dataset_mode hdf5 --gpu_ids -1 --input_nc 1 --output_nc 1
 
+python test.py --dataroot tv_synth.h5 --name tv_synth_mdl --model pix2pix --norm instance --direction BtoA --dataset_mode hdf5 --gpu_ids -1 --input_nc 1 --output_nc 1 --no_dropout
 """""
 
 class HDF5Dataset(BaseDataset):
@@ -26,7 +27,8 @@ class HDF5Dataset(BaseDataset):
         """
         BaseDataset.__init__(self, opt)
         self.AB_path = Path(opt.dataroot) # get the image directory
-        print(self.AB_path)
+        self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
+        self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
         with h5py.File(self.AB_path, 'r') as f:
             print(list(f.keys()))
             self.A_arr = f['synth_' + opt.phase][:]
@@ -55,7 +57,7 @@ class HDF5Dataset(BaseDataset):
         A = torch.from_numpy(A).float()
         B = torch.from_numpy(B).float()
 
-        return {'A': A, 'B': B, 'A_paths': str(self.AB_path.stem), 'B_paths': str(self.AB_path.stem)}
+        return {'A': A, 'B': B, 'A_paths': str(self.AB_path.stem) + '_A_' +  str(index), 'B_paths': str(self.AB_path.stem) + '_B_' +  str(index)}
 
     def __len__(self):
         """Return the total number of images in the dataset."""
